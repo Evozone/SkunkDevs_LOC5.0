5603cfbb869202940aa9e2d85a02604ffb86e3a5
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-03-11 16:14:37 +0530

    feat: Fix routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,88 +9,130 @@ import ProtectedRoute from './components/ProtectedRoute';
 import { signInAction } from './actions/actions';
 import MainAppbar from './components/MainAppbar';
 import Explore from './components/Explore';
-import Groups from './components/Groups';
 import Blogs from './components/Blogs';
+import Connect from './components/Connect';
+import Listings from './components/Listings';
+import Stage from './components/Stage';
 
 function App() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const localTheme = window.localStorage.getItem('healthAppTheme');
+  const localTheme = window.localStorage.getItem('healthAppTheme');
 
-    const [mode, setMode] = useState(localTheme ? localTheme : 'light');
+  const [mode, setMode] = useState(localTheme ? localTheme : 'light');
 
-    const darkTheme = createTheme({
-        palette: {
-            mode: mode,
-        },
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
 
-        typography: {
-            fontFamily: "'Open Sans', sans-serif",
-        },
-    });
+    typography: {
+      fontFamily: "'Open Sans', sans-serif",
+    },
+  });
 
-    const themeChange = () => {
-        const updatedTheme = mode === 'light' ? 'dark' : 'light';
-        window.localStorage.setItem('healthAppTheme', updatedTheme);
-        setMode(updatedTheme);
-    };
+  const themeChange = () => {
+    const updatedTheme = mode === 'light' ? 'dark' : 'light';
+    window.localStorage.setItem('healthAppTheme', updatedTheme);
+    setMode(updatedTheme);
+  };
 
-    const isSignedIn = true;
-    // const isSignedIn = useSelector((state) => state.auth.isSignedIn);
+  const isSignedIn = true;
+  // const isSignedIn = useSelector((state) => state.auth.isSignedIn);
 
-    useEffect(() => {
-        const auth = window.localStorage.getItem('photoApp');
-        if (auth) {
-            const { dnd } = JSON.parse(auth);
-            const { uid, email, name, photoURL, username, socialLinks } =
-                jwtDecode(dnd);
-            dispatch(
-                signInAction(
-                    uid,
-                    email,
-                    name,
-                    photoURL,
-                    username,
-                    socialLinks,
-                    dnd
-                )
-            );
-            const value = window.localStorage.getItem('photoApp');
-            if (value && value !== undefined) {
-                navigate(`/${value}`);
-            } else {
-                navigate('/');
-            }
-        }
-    }, []);
+  useEffect(() => {
+    const auth = window.localStorage.getItem('photoApp');
+    if (auth) {
+      const { dnd } = JSON.parse(auth);
+      const { uid, email, name, photoURL, username, socialLinks } =
+        jwtDecode(dnd);
+      dispatch(
+        signInAction(
+          uid,
+          email,
+          name,
+          photoURL,
+          username,
+          socialLinks,
+          dnd
+        )
+      );
+      const value = window.localStorage.getItem('photoApp');
+      if (value && value !== undefined) {
+        navigate(`/${value}`);
+      } else {
+        navigate('/explore');
+      }
+    }
+  }, []);
 
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <CssBaseline />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
 
-            <MainAppbar
-                {...{
-                    themeChange,
-                    mode,
-                }}
-            />
+      <MainAppbar
+        {...{
+          themeChange,
+          mode,
+        }}
+      />
 
-            <Routes>
-                <Route path='/' element={<Explore />} />
+      <Routes>
 
-                <Route
-                    path='/groups'
-                    element={<Groups themeChange={themeChange} mode={mode} />}
-                />
+        {/* Explore */}
 
-                <Route
-                    path='/blogs'
-                    element={<Blogs themeChange={themeChange} mode={mode} />}
-                />
-            </Routes>
-        </ThemeProvider>
-    );
+        <Route path='/explore' element={<Explore />} />
+
+        {/* Stage */}
+
+        <Route
+          path='/stage'
+          element={
+            <Stage themeChange={themeChange} mode={mode} />
+          }
+        />
+
+        {/* Blogs */}
+
+        <Route
+          path='/blogs'
+          element={
+            <Blogs themeChange={themeChange} mode={mode} />
+          }
+        />
+
+        {/* Connect */}
+
+        <Route
+          path='/connect'
+          element={
+            <Connect themeChange={themeChange} mode={mode} />
+          }
+        />
+
+        {/* Listings */}
+
+        <Route
+          path='/listings'
+          element={
+            <Listings themeChange={themeChange} mode={mode} />
+          }
+        />
+
+        {/* Profile */}
+
+        <Route
+          path='/profile'
+          element={
+            <Listings themeChange={themeChange} mode={mode} />
+          }
+        />
+
+      </Routes>
+
+    </ThemeProvider>
+  );
 }
 
 export default App;
